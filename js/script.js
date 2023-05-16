77aded1850d0d089cb1484e3cf46847d2cd87987
aymanmigal <aymanmigal.q@gmail.com>
2023-05-16 22:20:21 +0100

    FINE

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-let nome = prompt('Quale è il tuo nome?');
+let nome = prompt('Qual è il tuo nome?');
 
-let cognome = prompt('Quale è il tuo cognome?');
+let cognome = prompt('Qual è il tuo cognome?');
 
-let colore_preferito = prompt('Quale è il tuo colore preferito?');
+let colore_preferito = prompt('Qual è il tuo colore preferito?');
 
 const const_21 = '21';
 
